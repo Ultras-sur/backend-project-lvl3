7314236ac553d018209555b853e53a05ae4545c6
Ultras <ultras-sur@mail.ru>
2021-09-22 21:54:06 +0300

    New commit

---
+++ Makefile.mk
@@ -0,0 +1,17 @@
+install:
+        npm install
+
+publish:
+        npm publish --dry--run  
+
+lint:
+        npx eslint .
+
+push: 
+        git push -u origin main
+
+format:
+        npm run format
+
+test: 
+        npx -n '--experimental-vm-modules --no-warnings' jest  
